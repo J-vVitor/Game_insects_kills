f8e19bae97253c1efbe9423f2c3877c03f2b4beb
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2023-09-06 20:31:37 -0300

    f

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,12 @@
 var mosquito = document.querySelector('#mosquito');
 
+var altura = window.innerHeight - 50;
+var largura = window.innerWidth - 50;
+
+
+mosquito.addEventListener("mouseover", () => {
+    mosquito.style.position = "absolute"
+    mosquito.style.top = Math.random() * altura + "px";
+    mosquito.style.left =Math.random() * largura + "px";
+
+})
