13641f0693cf54e4efbcb4db03160aa8d2b4eb8a
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2023-09-07 15:46:23 -0300

    ajuste de tamanho/direção

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,50 @@
-var mosquito = document.querySelector('#mosquito');
+var mosquito = document.querySelector('.mosquito');
 
-var altura = window.innerHeight - 50;
-var largura = window.innerWidth - 50;
+var altura = 0;
+var largura = 0;
+
+function tamanhoPalco() {
+    altura = window.innerHeight - 90;
+    largura = window.innerWidth - 90;
+
+    console.log(altura, largura)
+}
+
+tamanhoPalco()
 
 
-mosquito.addEventListener("mouseover", () => {
+
+function passarCima() {
+
+    var posX = Math.floor(Math.random() * altura)
+    var posY = Math.floor(Math.random() * largura)
+
+    console.log(posX, posY)
+
     mosquito.style.position = "absolute"
-    mosquito.style.top = Math.random() * altura + "px";
-    mosquito.style.left =Math.random() * largura + "px";
+    mosquito.style.top = posX + "px"
+    mosquito.style.left = posY + "px"
+   
+    
+    
+    ladoMosquito();
+  
+}
 
-})
+function ladoMosquito(){
+    var lado = Math.floor(Math.random() * 2)
+
+    if(lado == 0 ){
+       mosquito.setAttribute('class','mosquitoD')
+    }else if( lado == 1){
+       mosquito.setAttribute('class','mosquitoE')
+    }
+}
+
+
+
+
+
+
+
+
