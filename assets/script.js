1fe5bc87cad5f30d073d2228a414579ac51919a4
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2023-09-08 15:04:02 -0300

    MODELANDO

--- assets/script.js
+++ assets/script.js
@@ -7,41 +7,22 @@ function tamanhoPalco() {
     altura = window.innerHeight - 90;
     largura = window.innerWidth - 90;
 
-    console.log(altura, largura)
+    console.log(altura, largura);
 }
 
 tamanhoPalco()
 
+function posicaoRandomica(){
 
-setInterval(function(){
-    passarCima()
-},1000)
+    var mosquito = document.querySelector('.mosquito');
+    var posX = Math.floor(Math.random() * largura);
+    var posY = Math.floor(Math.random() * altura);
 
-function passarCima() {
-
-    var posX = Math.floor(Math.random() * altura)
-    var posY = Math.floor(Math.random() * largura)
-
-    console.log(posX, posY)
+    console.log(posX, posY);
 
     mosquito.style.position = "absolute"
-    mosquito.style.top = posX + "px"
-    mosquito.style.left = posY + "px"
-   
-    
-    
-    ladoMosquito();
-  
-}
-
-function ladoMosquito(){
-    var lado = Math.floor(Math.random() * 2)
-
-    if(lado == 0 ){
-       mosquito.setAttribute('class','mosquitoD')
-    }else if( lado == 1){
-       mosquito.setAttribute('class','mosquitoE')
-    }
+    mosquito.style.top = posY + 'px'
+    mosquito.style.left = posX + 'px'
 }
 
 
