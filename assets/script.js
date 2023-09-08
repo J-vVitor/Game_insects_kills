b7c7c3211a54301597e37a3bb969cf615e80f02c
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2023-09-08 19:33:28 -0300

    Do zero

--- assets/script.js
+++ assets/script.js
@@ -8,27 +8,10 @@ function tamanhoPalco() {
     largura = window.innerWidth - 90;
 
     console.log(altura, largura);
+    
+
 }
 
 tamanhoPalco()
-
-function posicaoRandomica(){
-
-    var mosquito = document.querySelector('.mosquito');
-    var posX = Math.floor(Math.random() * largura);
-    var posY = Math.floor(Math.random() * altura);
-
-    console.log(posX, posY);
-
-    mosquito.style.position = "absolute"
-    mosquito.style.top = posY + 'px'
-    mosquito.style.left = posX + 'px'
-}
-
-
-
-
-
-
 
 
